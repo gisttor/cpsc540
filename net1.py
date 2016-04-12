ec8007fdc0c0211cc92203b754c7fe7a820426cb
Kelvin Ritland <kelrit@gmail.com>
2016-04-11 21:24:13 -0700

    Base image classifier
    68% F1 accuracy on images themselves
    Caches downsized images to HD

--- net1.py
+++ net1.py
@@ -6,51 +6,101 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
 from keras.utils import np_utils
 import read_data
 
-batch_size = 128
-nb_classes = 9
-nb_epoch = 12
+def main():
+    batch_size = 128
+    nb_classes = 9
+    nb_epoch = 3
+    dropout = 0.7
 
-# input image dimensions
-img_size = 100
-# number of convolutional filters to use
-nb_filters = 32
-# size of pooling area for max pooling
-nb_pool = 2
-# convolution kernel size
-nb_conv = 10
+    # input image dimensions
+    img_size = 150
+    # number of convolutional filters to use
+    nb_filters = 40
+    # size of pooling area for max pooling
+    nb_pool = 10 
+    # convolution kernel size
+    nb_conv = 9
 
-# the data, shuffled and split between train and test sets
-X_train, Y_train = read_data.read_data_photo_labels(img_size = img_size, num_biz = 100)
+    # the data, shuffled and split between train and test sets
+    (X_train, Y_train, X_test, Y_test) = \
+        read_data.read_data_photo_labels(img_size = img_size, num_biz = 150, fromfile = True)
 
-print('X_train shape:', X_train.shape)
-print('Y_train shape:', Y_train.shape)
+    print('X_train shape:', X_train.shape)
+    print('Y_train shape:', Y_train.shape)
 
-model = Sequential()
+  # datagen = ImageDataGenerator(
+  #     featurewise_center=False,
+  #     featurewise_std_normalization=False,
+  #     rotation_range=20,
+  #     zca_whitening=False,
+  #     width_shift_range=0.2,
+  #     height_shift_range=0.2,
+  #     horizontal_flip=True)
+  # datagen.fit(X_train)
 
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                        border_mode='valid',
-			input_shape=(3, img_size, img_size)))
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-model.add(Dropout(0.5))
+    model = Sequential()
 
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dense(256))
-model.add(Activation('relu'))
-model.add(Dense(nb_classes))
-model.add(Activation('tanh'))
+    model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                            border_mode='valid',
+                            input_shape=(3, img_size, img_size)))
+    model.add(Activation('relu'))
 
-model.compile(loss='mae', optimizer='adagrad')
+    model.add(MaxPooling2D(pool_size=(4,4)))
+    model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+    model.add(Activation('relu'))
 
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-	  show_accuracy=True, verbose=1, validation_split = 0.1)
-score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+    #model.add(MaxPooling2D(pool_size=(1, 1)))
+    #model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+    #model.add(Activation('relu'))
+
+    model.add(Flatten())
+    model.add(Dropout(dropout))
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+
+ #  model.add(Dropout(dropout))
+ #  model.add(Dense(128))
+ #  model.add(Activation('relu'))
+
+    model.add(Dropout(dropout))
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+
+    model.add(Dense(nb_classes))
+
+    model.compile(loss='hinge', optimizer='adadelta')
+
+    model.fit(
+            X_train, Y_train, batch_size=32,
+            nb_epoch=nb_epoch, verbose=1)
+
+ #  model.fit_generator(
+ #          datagen.flow(X_train, Y_train, batch_size=32), 
+ #          samples_per_epoch=18226, nb_epoch=nb_epoch, verbose=1)
+    test_pred = np.sign(model.predict(X_test))
+    test_loss = model.evaluate(X_test, Y_test)
+
+    print('Test loss: ', test_loss)
+    print('Test accuracy: ', accuracy(test_pred, Y_test))
+    print('F1 score: ', f1score(test_pred, Y_test))
+
+    # next: based on the photo predictions, what are the biz labels?
+
+def accuracy(y_pred, y_test):
+    total = y_test.size
+    correct = np.sum(y_pred == y_test)
+    return correct/total
+
+def f1score(y_pred, y_test):
+    tp = np.sum(np.logical_and(y_pred == 1, y_test == 1))
+    fp = np.sum(np.logical_and(y_pred == 1, y_test == -1))
+    fn = np.sum(np.logical_and(y_pred == -1, y_test == 1))
+    precision = tp/(tp + fp)
+    recall = tp/(tp + fn)
+    return 2*precision*recall/(precision + recall)
+
+main()
--- read_data.py
+++ read_data.py
@@ -7,17 +7,36 @@ from keras.utils import np_utils
 from keras.preprocessing import image
 from keras.utils.generic_utils import Progbar
 
+CACHE_XTRAIN = 'x_train.npy'
+CACHE_YTRAIN = 'y_train.npy'
+CACHE_XTEST  = 'x_test.npy'
+CACHE_YTEST  = 'y_test.npy'
+
 # convert data into test/validate/train
 # format: each business has a set of images
 
-def read_data_photo_labels(num_biz = 100, test_ratio = 0.1, img_size = 150, num_labels = 9):
+def load_cache():
+    out = []
+    for fname in [CACHE_XTRAIN, CACHE_YTRAIN, CACHE_XTEST, CACHE_YTEST]:
+        out.append(np.load(fname))
+    return tuple(out)
+
+def read_data_photo_labels(
+        num_biz = 100, img_size = 150, num_labels = 9, test_split = 0.1,
+        fromfile = True):
     ''' Read data, pushing labels down to photos
 
     return structure:
-    x_train, y_train
+    (x_train, y_train, x_test, y_test)
     x_train: image turned to vector
     y_train: categorical labels: binary vectors of size [num classes]
     '''
+    if fromfile:
+        try:
+            return load_cache()
+        except IOError:
+            pass
+    
     # construct dict of businesses:
     # structure {biz_id: (binary labels, list of photos)}
     biz_info = {}
@@ -25,7 +44,7 @@ def read_data_photo_labels(num_biz = 100, test_ratio = 0.1, img_size = 150, num_
         reader = csv.reader(csvfile)
         next(reader)   # skip header
         for row in reader:
-            labels = np.zeros(num_labels)
+            labels = np.zeros(num_labels) - 1
             for c in row[1].split():
                 labels[c] = 1
             biz_info[int(row[0])] = (labels, list())
@@ -38,7 +57,7 @@ def read_data_photo_labels(num_biz = 100, test_ratio = 0.1, img_size = 150, num_
 
     # shape: num_biz, 3 channels, img x, img y
     num_image = 0
-    biz_count = 0;
+    biz_count = 0
     for biz, (labels, photo_ids) in biz_info.items():
         num_image += len(photo_ids)
         biz_count += 1
@@ -46,30 +65,43 @@ def read_data_photo_labels(num_biz = 100, test_ratio = 0.1, img_size = 150, num_
             break
 
     print('Reading photos')
-    # using ones instead of zeros prevents memory from blowing up for some reason
-    x = np.ones((num_image, 3, img_size, img_size), dtype=np.float32)
-    y = np.ones((num_image, num_labels), dtype=np.int32)
+    # using ones instead of zeros prevents memory from blowing up for some 
+    # reason
+    # copy on write mechanics?
+    num_test = int(num_image*test_split)
+    num_train = num_image - num_test
+    x_train = np.ones((num_train, 3, img_size, img_size), dtype=np.float32)
+    y_train = np.ones((num_train, num_labels), dtype=np.int32)
+    x_test = np.ones((num_test, 3, img_size, img_size), dtype=np.float32)
+    y_test = np.ones((num_test, num_labels), dtype=np.int32)
     biz_count = 0;
     img_count = 0;
-    order = np.random.permutation(num_image) - 1
+    order = np.random.permutation(num_image)
     progbar = Progbar(num_image)
     for biz, (labels, photo_ids) in biz_info.items():
         for photo_id in photo_ids:
             im_raw = image.load_img('data/train_photos/%s.jpg' % photo_id)
-            im_raw = im_raw.resize((img_size, img_size), PIL.Image.NEAREST)
-            x[order[img_count]] = image.img_to_array(im_raw)
-            y[order[img_count]] = labels
+            im_raw = im_raw.resize((img_size, img_size), PIL.Image.ANTIALIAS)
+            index = order[img_count]
+            if index < num_train:
+                x_train[index] = image.img_to_array(im_raw)
+                y_train[index] = labels
+            else:
+                x_test[index - num_train] = image.img_to_array(im_raw)
+                y_test[index - num_train] = labels
             img_count += 1
             progbar.add(1)
         biz_count += 1
-        gc.collect()
         if biz_count >= num_biz:
             break
-    x = x/255; 
-    return x,y
+    
+    x_train /= 255; 
+    x_test /= 255; 
+    
+    for fname, arr in \
+        zip([CACHE_XTRAIN, CACHE_YTRAIN, CACHE_XTEST, CACHE_YTEST],
+            [x_train, y_train, x_test, y_test]):
+       np.save(fname, arr)
 
-def shuffle_in_unison(a, b):
-    rng_state = np.random.get_state()
-    np.random.shuffle(a)
-    np.random.set_state(rng_state)
-    np.random.shuffle(b)
+    return (x_train, y_train, x_test, y_test)
+
